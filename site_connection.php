b54ad0777dd4213a1ca6469e0c680ab8c7687a54
Anisha44 <79452101+Anisha44@users.noreply.github.com>
2025-03-17 16:06:24 +1300

    Update site_connection.php

--- site_connection.php
+++ site_connection.php
@@ -3,8 +3,14 @@
 session_start();
 ob_start();
 
-$conn = mysqli_connect('cozadb.mysql.database.azure.com','czdb','sterlingGmail@2020','cozastore');
+$host = 'cozadb.mysql.database.azure.com';
+$username = 'czdb';
+$password = 'sterlingGmail@2020'; // Use actual password
+$database = 'cozastore';
 
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL)
+mysqli_real_connect($conn, $host, $username, $password, $database, NULL , NULL, MYSQLI_CLIENT_SSL);
 
 
  ?>
