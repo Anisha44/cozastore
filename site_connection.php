a3c67f2443e544d63504b873127e76f5b31a92ec
Anisha44 <79452101+Anisha44@users.noreply.github.com>
2025-03-17 16:11:37 +1300

    Update site_connection.php

--- site_connection.php
+++ site_connection.php
@@ -9,7 +9,7 @@ $password = 'sterlingGmail@2020'; // Use actual password
 $database = 'cozastore';
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL)
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 mysqli_real_connect($conn, $host, $username, $password, $database, NULL , NULL, MYSQLI_CLIENT_SSL);
 
 
