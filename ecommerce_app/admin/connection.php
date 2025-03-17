ac1629534584ed73d3f97303ef28d6525094682f
Anisha44 <anishabaskota31@gmail.com>
2025-03-17 14:47:33 +1300

    initial commit

---
+++ ecommerce_app/admin/connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+session_start();
+ob_start();
+
+$conn = mysqli_connect('cozastore.c3mayimoi2f7.ap-southeast-2.rds.amazonaws.com','cozastore','mrFYV48AjEJOZTA566h0','cozastore');
+
+
+
+ ?>
