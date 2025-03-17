11fc004f71e57de8f04b5f1ede7430883ec5b40b
Anisha44 <79452101+Anisha44@users.noreply.github.com>
2025-03-17 15:51:24 +1300

    Update site_connection.php

--- site_connection.php
+++ site_connection.php
@@ -3,7 +3,7 @@
 session_start();
 ob_start();
 
-$conn = mysqli_connect('localhost','root','','cozastore');
+$conn = mysqli_connect('cozadb.mysql.database.azure.com','czdb','sterlingGmail@2020','cozastore');
 
 
 
